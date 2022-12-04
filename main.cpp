7e0c69152cb3e3d9aa7a0b7f008a4761f4c046fe
小夏 <2398771699@qq.com>
2022-12-04 22:24:55 +0800

    实现了sort函数

--- main.cpp
+++ main.cpp
@@ -4,7 +4,16 @@ using namespace std;
 
 void sort(int arr[], int size)
 {
-
+	for(int i = 0; i < size; i++)
+		for (int j = 0; j < size - 1 - i; j++)
+		{
+			if(arr[j] > arr[i])
+			{
+				int temp = arr[i];
+				arr[j] = arr[i];
+				arr[i] = temp;
+			}
+		}
 
 }
 
